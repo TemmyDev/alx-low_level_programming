97b186e10690abb607d82b60a1ad3cbe4198375e
Temilorun Lajorin <tlajorin@gmail.com>
2022-09-12 08:03:38 -0700

    added new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Descrption: prints all base numbers of 10
+ *
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
