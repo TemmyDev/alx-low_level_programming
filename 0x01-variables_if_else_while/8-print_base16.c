5f5a538e0ee01f2e075440f0713ef30ee83cc967
Temilorun Lajorin <tlajorin@gmail.com>
2022-09-12 08:40:40 -0700

    added code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description : prints
+ * Return: 0
+ */
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		c++;
+	}
+
+	char c = 'a';        
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
