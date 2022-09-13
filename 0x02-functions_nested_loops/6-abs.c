232c147429b6c3ddbb58583270468f11a26f91ba
Temilorun Lajorin <tlajorin@gmail.com>
2022-09-13 07:37:59 -0700

    added new code

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+ * _abs - absolute value
+ * @n: the int to print
+ * Return: always 0
+ */
+
+
+
+int _abs(int n)
+
+{
+
+
+
+	if (n < 0)
+
+	{
+
+		return (n * (-1));
+
+	}
+
+
+
+	else if (n == 0)
+
+	{
+
+		return (0);
+
+	}
+
+
+
+	else
+
+	{
+
+		return (n);
+
+	}
+
+
+
+}
